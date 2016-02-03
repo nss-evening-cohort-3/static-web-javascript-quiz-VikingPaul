0e13014b9b815f95bfb7b8a250298f008d8b5f66
Paul <wip321@yahoo.com>
2016-02-03 17:43:28 -0600

    got JavaScript writen and working.

---
+++ quiz.js
@@ -0,0 +1,49 @@
+var button = document.getElementById("treeMaker")
+function makeTree() {
+  var height = document.getElementById("height").value
+  var decoration = document.getElementById("character").value
+  var placement = document.getElementById("treeGrowsHere")
+  tester(height, decoration)
+  var tree = ""
+  for (i=0; i < height; i++) {
+  //adjusts height of tree
+  tree +="<p>"
+    for (j=0; j < i*2+1; j++) {
+      //creates all characters on line
+      tree += decoration
+    }
+  tree += "</p>"
+  }
+  console.log("tree", tree);
+  placement.innerHTML = tree
+  placement.style.textAlign = "center"
+}
+function tester(number, character) {
+  if (character === "") {
+    alert("Please use an actual character!")
+  } else if (isNaN(number)) {
+    alert("Please use an actual number!")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+button.addEventListener("click", makeTree)
